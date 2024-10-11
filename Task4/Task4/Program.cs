bb23b4d1cc79160f704b86a67ab151f9a2eb59cd
yosrzohair <153124314+yosrzohair@users.noreply.github.com>
2024-10-11 17:58:50 +0300

    Add files via upload

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        //1
+        //static double SumAndAverage(double sum , double average)
+        //{
+        //    sum = 0;
+        //    const int totalnumbers = 10;
+        //    for (int i=1; i < totalnumbers; i++)
+        //    {
+        //        Console.Write($"Number-{i}: ");
+        //        int number = int.Parse(Console.ReadLine());
+        //        sum += number; // Add each number to the sum
+        //         average = (double)sum / totalnumbers;
+        //    }
+
+        //return (sum, average);
+        //}
+
+
+
+        //2
+        static int cube (int x)
+        {
+            return x * x * x;
+        }
+
+        //3
+
+
+
+
+        static void Main(string[] args)
+        {
+            // Task4+5+6+7
+
+            //task4- Methods 
+            //1 
+            // Call the function and get the result
+            //(int sum, double average) = SumAndAverage();
+
+            //Display the sum and average
+            //Console.WriteLine(sum);
+            //Console.WriteLine(average);
+
+
+            int z = int.Parse(Console.ReadLine());
+            Console.WriteLine(z);
+
+            cube(z);
+
+        }
+    }
+}
